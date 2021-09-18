869b9d35e0a2dc882a34a613ff86a6e1fcb997db
frokofaron <mineev96@gmail.com>
2021-09-19 00:08:12 +0300

    добавлено получение списка лидербордов

--- main.py
+++ main.py
@@ -64,28 +64,49 @@ def main():
     date_to = date(2021, 9, 12)
     url_list = get_url_list(date_from, date_to)
 
-    htmls = loop.run_until_complete(fetch_all(url_list, loop))
-    soup = BeautifulSoup(htmls[0], 'html.parser')
+    vdt_list_html = loop.run_until_complete(fetch_all([base_url], loop))[0]
+    leaderboard_htmls = loop.run_until_complete(fetch_all(url_list, loop))
 
-    table = soup.find('table')
-    rows = table.find_all('tr')
-    rows.pop(0)  # удаление заголовка
-    flights = list()
-    for row in rows:
-        cells = row.find_all('td')
-        flight = Flight(
-            position=          int(cells[1].text),
-            country=               cells[2].img['title'],
-            player=                cells[2].text.lstrip(),
-            time=            float(cells[3].find(text=True, recursive=False)),
-            green_delta=     float(cells[3].sup.text or 0.0),
-            delta=           float(cells[4].text),
-            drone=                 cells[5].text,
-            date=datetime.strptime(cells[6].text, '%Y-%m-%d %H:%M'),
-            updates=           int(cells[7].text),
+    leaderboards = list()
+    soup = BeautifulSoup(vdt_list_html, 'html.parser')
+
+    delta = timedelta(days=1)
+    for i in range((date_to - date_from).days + 1):
+        current_date = date_from + i*delta
+        current_tag = soup.find(text=datetime.strftime(current_date, "%Y-%m-%d")).parent.parent
+        cells = current_tag.find_all('td')
+        leaderboard = Leaderboard(
+            season=int(cells[0].text),
+            date=current_date,
+            map=cells[2].text.split('/')[0].rstrip(),
+            track=cells[2].text.split('/')[1].lstrip()
         )
-        flights.append(flight)
-    pprint.pprint(flights)
+        leaderboards.append(leaderboard)
+
+    for leaderboard, html in zip(leaderboards, leaderboard_htmls):
+        soup = BeautifulSoup(html, 'html.parser')
+
+        table = soup.find('table')
+        rows = table.find_all('tr')
+        rows.pop(0)  # удаление заголовка
+        flights = list()
+        for row in rows:
+            cells = row.find_all('td')
+            flight = Flight(
+                position=          int(cells[1].text),
+                country=               cells[2].img['title'],
+                player=                cells[2].text.lstrip(),
+                time=            float(cells[3].find(text=True, recursive=False)),
+                green_delta=     float(cells[3].sup.text or 0.0),
+                delta=           float(cells[4].text),
+                drone=                 cells[5].text,
+                date=datetime.strptime(cells[6].text, '%Y-%m-%d %H:%M'),
+                updates=           int(cells[7].text),
+            )
+            flights.append(flight)
+        leaderboard.flights = flights
+
+    pprint.pp(leaderboards)
 
 
 if __name__ == '__main__':
