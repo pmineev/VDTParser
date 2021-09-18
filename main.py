b1200639a338f194808def38f6ee749b364e6b33
frokofaron <mineev96@gmail.com>
2021-09-19 00:36:02 +0300

    парсинг вынесен в отдельные функции

--- main.py
+++ main.py
@@ -35,22 +35,13 @@ async def fetch_all(urls, loop):
         return results
 
 
-def main():
-    loop = asyncio.get_event_loop()
-
-    date_from = date(2021, 9, 10)
-    date_to = date(2021, 9, 12)
-    url_list = get_url_list(date_from, date_to)
-
-    vdt_list_html = loop.run_until_complete(fetch_all([base_url], loop))[0]
-    leaderboard_htmls = loop.run_until_complete(fetch_all(url_list, loop))
-
-    leaderboards = list()
-    soup = BeautifulSoup(vdt_list_html, 'html.parser')
+def parse_vdt_list(html, date_from, date_to):
+    soup = BeautifulSoup(html, 'html.parser')
 
     delta = timedelta(days=1)
+    leaderboards = list()
     for i in range((date_to - date_from).days + 1):
-        current_date = date_from + i*delta
+        current_date = date_from + i * delta
         current_tag = soup.find(text=datetime.strftime(current_date, "%Y-%m-%d")).parent.parent
         cells = current_tag.find_all('td')
         leaderboard = Leaderboard(
@@ -61,27 +52,53 @@ def main():
         )
         leaderboards.append(leaderboard)
 
-    for leaderboard, html in zip(leaderboards, leaderboard_htmls):
-        soup = BeautifulSoup(html, 'html.parser')
+    return leaderboards
 
-        table = soup.find('table')
-        rows = table.find_all('tr')
-        rows.pop(0)  # удаление заголовка
-        flights = list()
-        for row in rows:
-            cells = row.find_all('td')
-            flight = Flight(
-                position=          int(cells[1].text),
-                country=               cells[2].img['title'],
-                player=                cells[2].text.lstrip(),
-                time=            float(cells[3].find(text=True, recursive=False)),
-                green_delta=     float(cells[3].sup.text or 0.0),
-                delta=           float(cells[4].text),
-                drone=                 cells[5].text,
-                date=datetime.strptime(cells[6].text, '%Y-%m-%d %H:%M'),
-                updates=           int(cells[7].text),
-            )
-            flights.append(flight)
+
+def parse_flights(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    rows.pop(0)  # удаление заголовка
+    flights = list()
+    for row in rows:
+        flight = parse_flight(row)
+        flights.append(flight)
+
+    return flights
+
+
+def parse_flight(row):
+    cells = row.find_all('td')
+    flight = Flight(
+        position=int(cells[1].text),
+        country=cells[2].img['title'],
+        player=cells[2].text.lstrip(),
+        time=float(cells[3].find(text=True, recursive=False)),
+        green_delta=float(cells[3].sup.text or 0.0),
+        delta=float(cells[4].text),
+        drone=cells[5].text,
+        date=datetime.strptime(cells[6].text, '%Y-%m-%d %H:%M'),
+        updates=int(cells[7].text),
+    )
+
+    return flight
+
+
+def main():
+    loop = asyncio.get_event_loop()
+
+    date_from = date(2021, 9, 10)
+    date_to = date(2021, 9, 12)
+    url_list = get_url_list(date_from, date_to)
+
+    vdt_list_html = loop.run_until_complete(fetch_all([base_url], loop))[0]
+    leaderboard_htmls = loop.run_until_complete(fetch_all(url_list, loop))
+
+    leaderboards = parse_vdt_list(vdt_list_html, date_from, date_to)
+
+    for leaderboard, html in zip(leaderboards, leaderboard_htmls):
+        flights = parse_flights(html)
         leaderboard.flights = flights
 
     pprint.pp(leaderboards)
