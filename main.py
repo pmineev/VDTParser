33cf74ac2284fe2b73d38f797db446b6d3258d0a
frokofaron <mineev96@gmail.com>
2021-09-18 21:41:05 +0300

    добавлено получение списка адресов по датам

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import pprint
 from dataclasses import dataclass
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from typing import List
 
 import aiohttp
@@ -8,7 +9,19 @@ import ssl
 
 from bs4 import BeautifulSoup
 
-url_list = ['https://vdt.the23.ru/?date=2021-09-17']
+base_url = 'https://vdt.the23.ru'
+
+
+def get_url_list(date_from, date_to):
+    url_list = list()
+
+    delta = timedelta(days=1)
+    for i in range((date_to - date_from).days):
+        current_date = date_from + i*delta
+        url = base_url + '/?date=' + datetime.strftime(current_date, "%Y-%m-%d")
+        url_list.append(url)
+
+    return url_list
 
 
 async def fetch(session, url):
@@ -46,9 +59,12 @@ class Leaderboard:
 
 def main():
     loop = asyncio.get_event_loop()
-    urls = url_list
-    htmls = loop.run_until_complete(fetch_all(urls, loop))
 
+    date_from = date(2021, 9, 10)
+    date_to = date(2021, 9, 12)
+    url_list = get_url_list(date_from, date_to)
+
+    htmls = loop.run_until_complete(fetch_all(url_list, loop))
     soup = BeautifulSoup(htmls[0], 'html.parser')
 
     table = soup.find('table')
@@ -56,7 +72,6 @@ def main():
     rows.pop(0)  # удаление заголовка
     flights = list()
     for row in rows:
-        print(row)
         cells = row.find_all('td')
         flight = Flight(
             position=          int(cells[1].text),
@@ -70,7 +85,7 @@ def main():
             updates=           int(cells[7].text),
         )
         flights.append(flight)
-    print(flights)
+    pprint.pprint(flights)
 
 
 if __name__ == '__main__':
