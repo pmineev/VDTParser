ea080aa532661bfd2af4f616ae645adf5878e243
frokofaron <mineev96@gmail.com>
2021-09-20 02:46:14 +0300

    добавлены обработчики событий

--- mainwindow.py
+++ mainwindow.py
@@ -2,12 +2,17 @@ import wx
 from wx import Frame, Panel, TextCtrl, BoxSizer, FlexGridSizer, GridSizer, StaticText, Button, Size
 from wx.adv import DatePickerCtrl
 from wxmplot import PlotPanel
+from datetime import date
 
 
 class Window(Frame):
 
     def __init__(self, parent=None, title='VDTParser'):
         Frame.__init__(self, parent, title=title)
+
+        self.nickname = None
+        self.date_from = None
+        self.date_to = None
 
         self.plot_rel = None
         self.plot_abs = None
@@ -25,13 +30,16 @@ class Window(Frame):
 
         nickname_control = TextCtrl(panel)
         nickname_control.SetHint('Введите никнейм')
+        nickname_control.Bind(wx.EVT_TEXT, self.OnNicknameChanged)
         controls_box.Add(nickname_control, flag=wx.ALL | wx.EXPAND, border=10)
 
         dates_box = BoxSizer(wx.HORIZONTAL)
         from_text = StaticText(panel, label='от:')
         date_from_control = DatePickerCtrl(panel)
+        date_from_control.Bind(wx.adv.EVT_DATE_CHANGED, self.OnDateFromChanged)
         to_text = StaticText(panel, label='до:')
         date_to_control = DatePickerCtrl(panel)
+        date_to_control.Bind(wx.adv.EVT_DATE_CHANGED, self.OnDateToChanged)
         dates_box.Add(from_text, flag=wx.ALIGN_CENTER)
         dates_box.Add(date_from_control, flag=wx.LEFT | wx.RIGHT, border=10)
         dates_box.Add(to_text, flag=wx.ALIGN_CENTER)
@@ -39,7 +47,9 @@ class Window(Frame):
         controls_box.Add(dates_box, flag=wx.ALL, border=10)
 
         start_button = Button(panel, label='вперед!!!')
+        start_button.Bind(wx.EVT_BUTTON, self.OnStartButtonPressed)
         controls_box.Add(start_button, flag=wx.ALL | wx.EXPAND, border=10)
+        start_button.Bind(wx.EVT_BUTTON, self.OnExportButtonPressed)
         export_button = Button(panel, label='экспорт в xls')
         controls_box.Add(export_button, flag=wx.ALL | wx.EXPAND, border=10)
 
@@ -59,3 +69,20 @@ class Window(Frame):
 
         panel.SetSizer(hbox)
 
+    def OnNicknameChanged(self, e):
+        self.nickname = e.GetEventObject().GetValue()
+        print(self.nickname)
+
+    def OnDateFromChanged(self, e):
+        d = e.GetEventObject().GetValue()
+        self.date_from = date(d.year, d.month+1, d.day)
+
+    def OnDateToChanged(self, e):
+        d = e.GetEventObject().GetValue()
+        self.date_to = date(d.year, d.month+1, d.day)
+
+    def OnStartButtonPressed(self, e):
+        pass
+
+    def OnExportButtonPressed(self, e):
+        pass
