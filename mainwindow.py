13d0283cfd3825233db5b013c7ca0beaeed9aa3d
frokofaron <mineev96@gmail.com>
2021-09-20 04:30:58 +0300

    добавлено отображение графиков

--- mainwindow.py
+++ mainwindow.py
@@ -3,6 +3,7 @@ from wx import Frame, Panel, TextCtrl, BoxSizer, FlexGridSizer, GridSizer, Stati
 from wx.adv import DatePickerCtrl
 from wxmplot import PlotPanel
 from datetime import date
+import matplotlib.dates as mdates
 
 from scrapers import get_leaderboards
 from calc import get_deltas
@@ -17,8 +18,8 @@ class Window(Frame):
         self.date_from = date.today()
         self.date_to = date.today()
 
-        self.plot_rel = None
-        self.plot_abs = None
+        self.rel_plot = None
+        self.abs_plot = None
 
         self.init()
         self.SetMinSize(Size(800, 600))
@@ -61,9 +62,11 @@ class Window(Frame):
         plots_box = GridSizer(1, 0, 0)
 
         rel_plot = PlotPanel(panel, show_config_popup=False, size=(1000, 1000))
+        self.rel_plot = rel_plot
         plots_box.Add(rel_plot, flag=wx.EXPAND, proportion=1)
 
         abs_plot = PlotPanel(panel, show_config_popup=False, size=(1000, 1000))
+        self.abs_plot = abs_plot
         plots_box.Add(abs_plot, flag=wx.EXPAND, proportion=1)
 
         hbox.Add(plots_box, flag=wx.EXPAND)
@@ -87,6 +90,11 @@ class Window(Frame):
     def OnStartButtonPressed(self, e):
         leaderboards = get_leaderboards(self.date_from, self.date_to)
         deltas = get_deltas(leaderboards, self.nickname)
+
+        self.rel_plot.plot(list(deltas['rel_vdt'].keys()), deltas['rel_vdt'].values(), labelfontsize=5)
+        self.rel_plot.oplot(list(deltas['rel_world'].keys()), deltas['rel_world'].values(), labelfontsize=5)
+        self.abs_plot.plot(list(deltas['abs_vdt'].keys()), deltas['abs_vdt'].values(), labelfontsize=5)
+        self.abs_plot.oplot(list(deltas['abs_world'].keys()), deltas['abs_world'].values(), labelfontsize=5)
 
     def OnExportButtonPressed(self, e):
         pass
