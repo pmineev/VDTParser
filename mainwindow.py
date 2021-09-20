7d4770ee1a69b8c7c516763476ada41b53dd62ce
frokofaron <mineev96@gmail.com>
2021-09-21 02:31:46 +0300

    удалены отладочные данные

--- mainwindow.py
+++ mainwindow.py
@@ -8,58 +8,6 @@ from calc import get_deltas
 
 from plotcanvas import PlotCanvas
 
-import datetime
-
-deltas = {'dates': [datetime.date(2021, 9, 8),
-                    datetime.date(2021, 9, 9),
-                    datetime.date(2021, 9, 13),
-                    datetime.date(2021, 9, 16),
-                    datetime.date(2021, 9, 17),
-                    datetime.date(2021, 9, 19)],
-          'abs_vdt': [10.26,
-                      8.322,
-                      11.613,
-                      11.586,
-                      13.541,
-                      9.928],
-          'abs_world': [10.260000000000002,
-                        8.322000000000001,
-                        15.979,
-                        11.586,
-                        13.621000000000002,
-                        9.928],
-          'rel_vdt': [55.1672222819658, 52.435259277928296,
-                      59.132338713783795,
-                      78.15704263356719, 75.77504196978175,
-                      52.82818070558187],
-          'rel_world': [55.16722228196581,
-                        52.4352592779283,
-                        104.62253650232437,
-                        78.15704263356719,
-                        76.56548622821812,
-                        52.82818070558187]}
-deltas2 = {'dates': [datetime.date(2021, 9, 1), datetime.date(2021, 9, 2), datetime.date(2021, 9, 3),
-                     datetime.date(2021, 9, 6), datetime.date(2021, 9, 8), datetime.date(2021, 9, 9),
-                     datetime.date(2021, 9, 10), datetime.date(2021, 9, 12), datetime.date(2021, 9, 13),
-                     datetime.date(2021, 9, 14), datetime.date(2021, 9, 15), datetime.date(2021, 9, 16),
-                     datetime.date(2021, 9, 17), datetime.date(2021, 9, 18), datetime.date(2021, 9, 19),
-                     datetime.date(2021, 9, 20)],
-           'abs_vdt': [7.024, 7.302, 7.831, 6.805, 6.375, 5.981, 5.994, 5.577, 3.04, 6.006, 7.424, 5.789, 7.744, 9.094,
-                       4.945, 4.381],
-           'abs_world': [7.024000000000001, 7.302000000000003, 7.8309999999999995, 6.805, 6.375, 5.981,
-                         5.9940000000000015, 5.577000000000002, 7.405999999999999, 6.006, 9.677000000000001, 5.789,
-                         7.824000000000002, 9.094000000000001, 4.945, 4.381000000000002],
-           'rel_vdt': [40.863342835534354, 41.378137927126424, 43.52006224296988, 40.72411729503291, 34.27787934186472,
-                       37.685086005922756, 42.88167119759622, 31.060985797827907, 15.47940322827028, 48.40815668574192,
-                       41.0892185078592, 39.051538046411224, 43.33519865696698, 51.3640214628636, 26.312988878837867,
-                       32.982007076714595],
-           'rel_world': [40.86334283553436, 41.378137927126446, 43.52006224296988, 40.72411729503291, 34.27787934186472,
-                         37.685086005922756, 42.88167119759623, 31.060985797827918, 48.490800759510236,
-                         48.40815668574192, 61.18874486247235, 39.051538046411224, 43.9797639123103, 51.364021462863604,
-                         26.312988878837867, 32.982007076714616]}
-
-tick = True
-
 
 class MainWindow(Frame):
 
@@ -129,21 +77,10 @@ class MainWindow(Frame):
         self.date_to = date(d.year, d.month + 1, d.day)
 
     def OnStartButtonPressed(self, e):
-        # leaderboards = get_leaderboards(self.date_from, self.date_to)
-        # deltas = get_deltas(leaderboards, self.nickname)
+        leaderboards = get_leaderboards(self.date_from, self.date_to)
+        deltas = get_deltas(leaderboards, self.nickname)
 
-        global tick
-        if tick:
-            self.plot_canvas.plot_deltas(deltas)
-            tick = not tick
-        else:
-            self.plot_canvas.plot_deltas(deltas2)
-            tick = not tick
-
-        # self.plot_canvas.plot(0, deltas['dates'], deltas['rel_vdt'])
-        # self.plot_canvas.plot(0, deltas['dates'], deltas['rel_world'])
-        # self.plot_canvas.plot(1, deltas['dates'], deltas['abs_vdt'])
-        # self.plot_canvas.plot(1, deltas['dates'], deltas['abs_world'])
+        self.plot_canvas.plot_deltas(deltas)
 
     def OnExportButtonPressed(self, e):
         pass
