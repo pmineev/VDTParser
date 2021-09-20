cf0f170c949836ed7e249e9d012ea238de8a45a8
frokofaron <mineev96@gmail.com>
2021-09-21 02:29:00 +0300

    радикально улучшено отображение графиков

--- calc.py
+++ calc.py
@@ -15,46 +15,50 @@ def filter_leaderboards(leaderboards, nickname):
 
 
 def get_abs_vdt_deltas(leaderboards, nickname):
-    deltas = dict()
+    deltas = list()
     for leaderboard in leaderboards:
         flight = [f for f in leaderboard.flights if f.player == nickname][0]
-        deltas[leaderboard.date] = flight.delta
+        deltas.append(flight.delta)
 
     return deltas
 
 
 def get_abs_world_deltas(leaderboards, nickname):
-    deltas = dict()
+    deltas = list()
     for leaderboard in leaderboards:
         flight = [f for f in leaderboard.flights if f.player == nickname][0]
-        deltas[leaderboard.date] = flight.time - leaderboard.world_record
+        deltas.append(flight.time - leaderboard.world_record)
 
     return deltas
 
 
 def get_rel_vdt_deltas(leaderboards, nickname):
-    deltas = dict()
+    deltas = list()
     for leaderboard in leaderboards:
         flight = [f for f in leaderboard.flights if f.player == nickname][0]
-        deltas[leaderboard.date] = flight.delta / leaderboard.flights[0].time * 100
+        deltas.append(flight.delta / leaderboard.flights[0].time * 100)
 
     return deltas
 
 
 def get_rel_world_deltas(leaderboards, nickname):
-    deltas = dict()
+    deltas = list()
     for leaderboard in leaderboards:
         flight = [f for f in leaderboard.flights if f.player == nickname][0]
-        deltas[leaderboard.date] = (flight.time - leaderboard.world_record) / leaderboard.world_record * 100
+        deltas.append((flight.time - leaderboard.world_record) / leaderboard.world_record * 100)
 
     return deltas
 
 
 def get_deltas(leaderboards, nickname):
     filtered_leaderboards = filter_leaderboards(leaderboards, nickname)
-    return {
+    dates = [lb.date for lb in filtered_leaderboards]
+    deltas = {
+        'dates': dates,
         'abs_vdt': get_abs_vdt_deltas(filtered_leaderboards, nickname),
         'abs_world': get_abs_world_deltas(filtered_leaderboards, nickname),
         'rel_vdt': get_rel_vdt_deltas(filtered_leaderboards, nickname),
         'rel_world': get_rel_world_deltas(filtered_leaderboards, nickname),
     }
+
+    return deltas
--- plotcanvas.py
+++ plotcanvas.py
@@ -1,4 +1,5 @@
 import wx
+from matplotlib.dates import DateFormatter
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 
@@ -7,9 +8,11 @@ class PlotCanvas(wx.Window):
     def __init__(self, parent):
         wx.Window.__init__(self, parent, -1)
 
-        self.figure = Figure()
+        self.figure = Figure(tight_layout=True)
+
         self.axes = (self.figure.add_subplot(211),
                      self.figure.add_subplot(212))
+        self.axes[0].set_xticks([])
 
         self.canvas = FigureCanvas(self, -1, self.figure)
 
@@ -17,6 +20,31 @@ class PlotCanvas(wx.Window):
         self.sizer.Add(self.canvas, 1, flag=wx.EXPAND)
         self.SetSizer(self.sizer)
 
-    def plot(self, index, *args, **kwargs):
-        self.axes[index].plot(*args, **kwargs)
+    def plot_deltas(self, deltas):
+        self.axes[0].clear()
+        self.axes[1].clear()
+
+        self.axes[0].set_title('Проценты всоса')
+        self.axes[1].set_title('Абсолютные всосы')
+
+        self.axes[0].plot(deltas['dates'], deltas['rel_vdt'])
+        self.axes[0].plot(deltas['dates'], deltas['rel_world'])
+        self.axes[1].plot(deltas['dates'], deltas['abs_vdt'])
+        self.axes[1].plot(deltas['dates'], deltas['abs_world'])
+
+        self.axes[0].set_yticks(deltas['rel_vdt'] + deltas['rel_world'], minor=True)
+        self.axes[1].set_yticks(deltas['abs_vdt'] + deltas['abs_world'], minor=True)
+
+        self.axes[0].set_xticks(deltas['dates'])
+        self.axes[1].set_xticks(deltas['dates'])
+        self.axes[1].xaxis.set_major_formatter(DateFormatter('%d.%m.%Y'))
+
+        self.axes[0].grid(axis='x')
+        self.axes[0].grid(axis='y', which='minor')
+        self.axes[1].grid(axis='x')
+        self.axes[1].grid(axis='y', which='minor')
+
+        self.axes[0].legend(('VDT', 'По миру'))
+
+        self.figure.autofmt_xdate(rotation=45)
         self.canvas.draw()
